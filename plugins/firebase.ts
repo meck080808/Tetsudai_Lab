e0b6935c9e4cf982f174556d2c9249eab6027d99
humminghorse <r.imahashi0724@gmail.com>
2020-12-11 11:42:12 +0900

    サンプルプロジェクト用に作成したファイルを削除

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -5,13 +5,7 @@ import 'firebase/storage'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyAZk4BIcu84kIK72SEkHSnafuZTP-7TCS8",
-    authDomain: "titech-2020-imahashi.firebaseapp.com",
-    databaseURL: "https://titech-2020-imahashi.firebaseio.com",
-    projectId: "titech-2020-imahashi",
-    storageBucket: "titech-2020-imahashi.appspot.com",
-    messagingSenderId: "454271650",
-    appId: "1:454271650:web:2252a5355e5f24d6052dfb"
+    // TODO: insert configuration
   })
 }
 
