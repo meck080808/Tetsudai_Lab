d60fa1ed181624ef9fcbc1a3f501d873147753ae
丸山拓幸 <takuyuki@maruyamatakukounoMacBook-Pro.local>
2020-12-13 14:05:28 +0900

    idnew

--- pages/index.vue
+++ pages/index.vue
@@ -5,43 +5,30 @@
             href="/"
             class="text-xl sm:text-2xl font-bold uppercase text-blue-900 hover:text-blue-800"
         >
-            <img src="@/assets/img/logo.png" alt="画像が見つかりません" style="width: 1000px;">
+            <img src="@/assets/img/logo.png" alt="画像が見つかりません" style="width: 1000px;" class="m-auto">
         </a>
     </p>
-    <h1 class="text-2xl sm:text-3xl text-blue-900 p-4 mb-0 mt-10 text-center">
+    <h1 class="text-2xl sm:text-3xl text-blue-900 p-4 mb-0 mt-10 text-center animate-bounce">
       研究アルバイトを探す
     </h1>
     <div class="text-center items-center">
         <input type="search" placeholder="キーワードを入力" class="border w-full py-1 px-5 rounded">
         <a href="search">
-        <input type="submit" value="検索" class="w-100 bg-gray-500 hover:bg-gray-600 text-white font-bold py-1 px-5 rounded focus:outline-none focus:shadow-outline">
+        <input type="submit" value="検索" class="w-100 bg-gray-500 hover:bg-gray-600 text-white font-bold cursor-pointer py-2 px-10 rounded focus:outline-none focus:shadow-outline mt-4">
         </a>
     </div>
-    <div class="text-center">
-        <div class ="inline-block">
-            <h1 class="text-2xl sm:text-3xl text-blue-900 p-4 mb-0 mt-10">
+
+        <div class ="text-center m-auto">
+            <h1 class="text-2xl sm:text-3xl text-blue-900 p-4 mb-0 mt-4">
             カテゴリから探す
             </h1>
-            <div class="lg:w-11/12 mx-auto flex flex-wraps">
-            <categoryTable
-                class=" w-full text-center"
-            />
-            </div>
         </div>
-        <!-- <div class="inline-block">
-            <PageHeading class="text-center mt-10">
-            スキルから探す
-            </PageHeading>
-            <div class="lg:w-11/12 mx-auto flex flex-wraps">
-            <categoryTable
-                class=" w-full text-center"
-            />
-            </div>
-        </div> -->
-    </div>
-
+        <div class="mt-5">
+            <categoryTable/>
+        </div>
   </div>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'nuxt-composition-api'
 import PageHeading from '@/components/page-heading.vue'
